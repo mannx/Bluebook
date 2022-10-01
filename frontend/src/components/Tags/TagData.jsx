4d4bc54d4e7aa91c27353e816044255fcfba024e
Mannx <18175956+mannx@users.noreply.github.com>
2022-10-01 13:08:43 -0300

    tag bug fixed for null tag list

--- frontend/src/components/Tags/TagData.jsx
+++ frontend/src/components/Tags/TagData.jsx
@@ -1,77 +1,77 @@
 import React from "react";
 import UrlGet from "../URLs/URLs.jsx";
 
-class TagData extends React.Component {
-		constructor(props) {
-				super(props);
+export default class TagData extends React.Component {
+	constructor(props) {
+		super(props);
 
-				this.state = {
-						loading: true,
-						data: null,
-				}
+		this.state = {
+			loading: true,
+			data: null,
+		}
+	}
+
+	loadData = async () => {
+		// loads the currently selected tag data from the server
+		if(this.props.tagid === null) return;			// nothing to load
+
+		const url = UrlGet("TagData") + "?id=" + this.props.tagid;
+		const resp = await fetch(url);
+		const data = await resp.json();
+
+		this.setState({data: data, loading: false});
+	}
+
+	componentDidMount() {
+		this.loadData();
+	}
+
+	componentDidUpdate(newProps) {
+		if(newProps.tagid !== this.props.tagid) {
+		this.loadData();
+		}
+	}
+
+	render() {
+		return (
+			<fieldset><legend>Tag Result</legend>
+				{this.showResult()}
+			</fieldset>
+		);
+	}
+
+	showResult = () => {
+		if(this.state.loading || this.state.data == null){
+			return <h1>Loading tag data...</h1>;
 		}
 
-		loadData = async () => {
-				// loads the currently selected tag data from the server
-				if(this.props.tagid === null) return;			// nothing to load
+		return (
+			<table>
+				<thead><tr>
+					<th>Date</th>
+					<th>Net Sales</th>
+					<th>Comments</th>
+					<th>Tags</th>
+				</tr></thead>
+				<tbody>
+					{this.state.data.map(function(obj,i) {
+						return (<tr>
+							<td>{obj.Date}</td>
+							<td>{obj.Day.NetSales}</td>
+							<td>{obj.Day.Comment}</td>
+							<td>{this.tag(obj.Tags)}</td>
+						</tr>);
+					}, this)}
+				</tbody>
+			</table>
+		);
+	}
 
-				const url = UrlGet("TagData") + "?id=" + this.props.tagid;
-				const resp = await fetch(url);
-				const data = await resp.json();
+	tag = (lst) => {
+		if(lst === null) { return; }
 
-				this.setState({data: data, loading: false});
-		}
-
-		componentDidMount() {
-				this.loadData();
-		}
-
-		componentDidUpdate(newProps) {
-				if(newProps.tagid !== this.props.tagid) {
-					this.loadData();
-				}
-		}
-
-		render() {
-				return (
-						<fieldset><legend>Tag Result</legend>
-								{this.showResult()}
-						</fieldset>
-				);
-		}
-
-		showResult = () => {
-				if(this.state.loading || this.state.data == null){
-						return <h1>Loading tag data...</h1>;
-				}
-
-				return (
-						<table>
-								<thead><tr>
-										<th>Date</th>
-										<th>Net Sales</th>
-										<th>Comments</th>
-										<th>Tags</th>
-								</tr></thead>
-								<tbody>
-										{this.state.data.map(function(obj,i) {
-												return (<tr>
-														<td>{obj.Date}</td>
-														<td>{obj.Day.NetSales}</td>
-														<td>{obj.Day.Comment}</td>
-														<td>{this.tag(obj.Tags)}</td>
-												</tr>);
-										}, this)}
-								</tbody>
-						</table>
-				);
-		}
-
-		tag = (lst) => {
-				return lst.map(function(obj, i) {
-						return <button className="tagButton">#{obj} </button>;
-				});
-		}
+		return lst.map(function(obj, i) {
+			return <button className="tagButton">#{obj} </button>;
+		});
+	}
 }
-
-export default TagData;
--- frontend/src/components/Tags/ViewTags.jsx
+++ frontend/src/components/Tags/ViewTags.jsx
@@ -5,56 +5,54 @@ import UrlGet from "../URLs/URLs.jsx";
 import "./style.css";
 
 // Display the list of tags and if/when provided, the days that tag was used on
-class ViewTags extends React.Component {
-		constructor(props) {
-				super(props);
-	
-				this.state = {
-						currentTag: null,				// id of the current tag we are showing
-						tagList: null,					// list of all tag data we have
-						loading: true,
-				}
+export default class ViewTags extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentTag: null,				// id of the current tag we are showing
+			tagList: null,					// list of all tag data we have
+			loading: true,
+		}
+	}
+
+	loadData = async () => {
+		// load the list of tags we have
+		const url = UrlGet("Tags");
+		const resp = await fetch(url);
+		const data = await resp.json();
+
+		this.setState({tagList: data, loading: false});
+	}
+
+	componentDidMount() {
+		this.loadData();
+	}
+
+	render() {
+		if(this.state.loading || this.state.tagList == null) {
+			return <h1>Loading tag data...</h1>;
 		}
 
-		loadData = async () => {
-				// load the list of tags we have
-				const url = UrlGet("Tags");
-				const resp = await fetch(url);
-				const data = await resp.json();
+		return (<>
+			{this.tagList()}
+			<TagData tagid={this.state.currentTag} />
+		</>);
+	}
 
-				this.setState({tagList: data, loading: false});
-		}
+	tagList = () => {
+		return (
+			<fieldset><legend>All Tags</legend>
+				<div><ul className={"tagList"}>
+				{this.state.tagList.map(function(obj, i) {
+					return <li><button  className='tag' onClick={() => this.viewTag(obj.ID) }>{obj.Tag}</button></li>;
+				}, this)}
+				</ul></div>
+			</fieldset>
+		);
+	}
 
-		componentDidMount() {
-				this.loadData();
-		}
-
-		render() {
-				if(this.state.loading || this.state.tagList == null) {
-						return <h1>Loading tag data...</h1>;
-				}
-
-				return (<>
-						{this.tagList()}
-						<TagData tagid={this.state.currentTag} />
-				</>);
-		}
-
-		tagList = () => {
-				return (
-						<fieldset><legend>All Tags</legend>
-								<div><ul className={"tagList"}>
-								{this.state.tagList.map(function(obj, i) {
-										return <li><button  className='tag' onClick={() => this.viewTag(obj.ID) }>{obj.Tag}</button></li>;
-								}, this)}
-								</ul></div>
-						</fieldset>
-				);
-		}
-
-		viewTag = (n) => {
-				this.setState({currentTag: n});
-		}
+	viewTag = (n) => {
+		this.setState({currentTag: n});
+	}
 }
-
-export default ViewTags;
