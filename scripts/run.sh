f7d8ccdbdaaf51facb4c1548e56514b2f62e85ba
mannx <18175956+mannx@users.noreply.github.com>
2023-03-03 17:52:44 -0400

    run script update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# copy over the remove-bk.sh script into the /data directory allowing user to cleanup previous backups
+cp /scripts/remove-bk.sh /data/remove-bk.sh
+
 # create a backup before we start
 /scripts/backup.sh
 
