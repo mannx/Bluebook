ccf2fd997c5da5cf9064af44c2df774ea380b64d
Mannx <18175956+mannx@users.noreply.github.com>
2025-01-16 20:58:15 -0400

    .

--- frontend2/src/components/Month/MonthView.jsx
+++ frontend2/src/components/Month/MonthView.jsx
@@ -5,9 +5,10 @@ import { UrlApiMonth, UrlApiSettingsGet } from "../URLs.jsx";
 // make sure we have the correct css for our table
 import "./month.css";
 
+import dayjs from "dayjs";
+
 // retrieve the month/year we want to see
 export async function loader({ params }) {
-  // const url = UrlApiMonth + "?month=" + params.month + "&year=" + params.year;
   const url = UrlApiMonth + "/" + params.month + "/" + params.year;
   const resp = await fetch(url);
   const data = await resp.json();
@@ -17,7 +18,8 @@ export async function loader({ params }) {
   // const settings = await r2.json();
   //
   // return { data, settings };
-  return { data };
+  params.month = params.month - 1; // adjust since dayjs using a 0 based month system
+  return { data, params };
 }
 
 // /today needs to redirect to the correct page for the current date
@@ -29,21 +31,20 @@ export async function today() {
 }
 
 export default function MonthView() {
-  const { data, settings } = useLoaderData();
+  const { data, params } = useLoaderData();
 
   // month navigation buttons are to be shows beside the month instead of the main navigation menu as previously done
+  const date = dayjs(new Date(params.year, params.month, 1));
   return (
     <table className="Month">
       <caption>
         <h3 className="no-print">
-          <span>{prevMonth(data)} </span>
-          <span>{nextMonth(data)} </span>
-          <span>{prevYear(data)} </span>
-          <span>{nextYear(data)}</span>
+          <span>{prevMonth(date)} </span>
+          <span>{nextMonth(date)} </span>
+          <span>{prevYear(date)} </span>
+          <span>{nextYear(date)}</span>
         </h3>
-        <h1>
-          {data.MonthName} {data.Year}
-        </h1>
+        <h1>{date.format("MMMM YYYY")}</h1>
       </caption>
       <thead>
         <tr>
@@ -86,10 +87,8 @@ export default function MonthView() {
           const row = Row(obj);
           let eow = null;
 
-          // if (obj.IsEndOfWeek) {
           if (obj.EndOfWeek !== null) {
             // show table data & end of week row
-            console.log("end of week");
             eow = EndOfWeek(obj);
           }
 
@@ -142,37 +141,53 @@ function Zero(obj) {
   return s.substring(s.length - 2, s.length);
 }
 
+function get_hockey_tag(data) {
+  // if (data.Hockey != null || data.Hockey !== undefined) {
+  if (data.Hockey) {
+    const url = "/" + data.Hockey.AwayImage;
+    const img = <img className="team-logo" src={url} />;
+    // let hock = data.Hockey.HomeGame === true ? img : <></>;
+    return data.Hockey.HomeGame === true ? img : <></>;
+  } else {
+    return <></>;
+  }
+}
+
 function Row(data) {
   let cls = "";
-  // let url = "/" + data.Hockey.AwayImage;
-  // let img = <img className="team-logo" src={url} />;
-  // let hock = data.Hockey.HomeGame === true ? img : <></>;
-  let hock = <></>;
+
+  const hock = get_hockey_tag(data);
 
   let hcls = "";
   let htip = "";
 
-  // if (data.Hockey.HomeGame === true) {
-  //   if (data.Hockey.GFHome === 0 && data.Hockey.GFAway === 0) {
-  //     // game not yet played, skip
-  //   } else if (data.Hockey.GFHome > data.Hockey.GFAway) {
-  //     hcls = "HockeyWin";
-  //   } else {
-  //     hcls = "HockeyLoss";
-  //   }
-  //
-  //   htip = (
-  //     <>
-  //       <span>
-  //         {data.Hockey.Away}
-  //         <br />
-  //       </span>
-  //       <span>
-  //         Home: {data.Hockey.GFHome} Away: {data.Hockey.GFAway}
-  //       </span>
-  //     </>
-  //   );
-  // }
+  // if (data.Hockey !== null || data.Hockey !== undefined) {
+  if (data.Hockey) {
+    console.log("hockey");
+    if (data.Hockey.HomeGame === true) {
+      if (data.Hockey.GFHome === 0 && data.Hockey.GFAway === 0) {
+        // game not yet played, skip
+      } else if (data.Hockey.GFHome > data.Hockey.GFAway) {
+        hcls = "HockeyWin";
+      } else {
+        hcls = "HockeyLoss";
+      }
+
+      console.log("hockey home game");
+
+      htip = (
+        <>
+          <span>
+            {data.Hockey.Away}
+            <br />
+          </span>
+          <span>
+            Home: {data.Hockey.GFHome} Away: {data.Hockey.GFAway}
+          </span>
+        </>
+      );
+    }
+  }
 
   switch (data.SalesLastWeek) {
     case 1:
@@ -186,10 +201,12 @@ function Row(data) {
       break;
   }
 
+  const dj = dayjs(new Date(data.Year, data.Month - 1, data.Day));
+
   return (
     <tr key={data.ID}>
       <td className="Month">{Zero(data.Day)}</td>
-      <td className="Month">{data.DayOfWeek}</td>
+      <td className="Month">{dj.format("dddd")}</td>
       <td className="Month">{O(data.GrossSales)}</td>
       <td className="Month">{O(data.Data.Hst)}</td>
       <td className="Month">{O(data.Data.BottleDeposit)}</td>
@@ -225,7 +242,7 @@ function Row(data) {
         </div>
       </td>
       <td className="Month">{data.Data.CommentData}</td>
-      <td className="Month no-print">{Tag(data.Tags, data.TagID)}</td>
+      <td className="Month no-print">{Tag(data.Tags)}</td>
       <td className="Month no-print">
         {data.ID !== 0 ? (
           <Link to={"/edit/" + data.Month + "/" + data.Year + "/" + data.ID}>
@@ -289,12 +306,12 @@ function EndOfWeek(data) {
 
 // Tag builds the user friendly display of all tags on this day
 // returns as a string
-function Tag(data, ids) {
+function Tag(data) {
   if (data !== null) {
     return (
       <>
-        {data.map((o, i) => {
-          return <Link to={"/tags/" + ids[i]}>#{o}</Link>;
+        {data.map((o) => {
+          return <Link to={"/tags/" + o.id}>#{o.tag}</Link>;
         })}
       </>
     );
@@ -304,49 +321,36 @@ function Tag(data, ids) {
 }
 
 // return a link for the next month
-function nextMonth(data) {
-  var month = data.Month + 1;
-  var year = data.Year;
-
-  if (month > 12) {
-    // move to next year
-    month = 1;
-    year = year + 1;
-  }
-
-  return monthNavLink(month, year, "Next");
+function nextMonth(date) {
+  const d = date.add(1, "month");
+  return monthNavLink(d, "Next");
 }
 
 // return a link to the previous month
-function prevMonth(data) {
-  var month = data.Month - 1;
-  var year = data.Year;
-
-  if (month < 1) {
-    // wrap to previous year
-    month = 12;
-    year = year - 1;
-  }
-
-  return monthNavLink(month, year, "Prev");
+function prevMonth(date) {
+  const d = date.subtract(1, "month");
+  return monthNavLink(d, "Prev");
 }
 
-function prevYear(data) {
-  return monthNavLink(data.Month, data.Year - 1, "Prev Year");
+function prevYear(date) {
+  const d = date.subtract(1, "year");
+  return monthNavLink(d, "Prev Year");
 }
 
-function nextYear(data) {
+function nextYear(date) {
   const currentYear = new Date().getFullYear();
 
-  if (data.Year !== currentYear) {
-    return monthNavLink(data.Month, data.Year + 1, "Next Year");
+  if (date.year() !== currentYear) {
+    const d = date.add(1, "year");
+    return monthNavLink(d, "Next Year");
   }
 
   return <></>;
 }
 // return the nav link for the given data
-function monthNavLink(month, year, str) {
-  const url = "/" + month + "/" + year;
+function monthNavLink(date, str) {
+  const month = date.month() + 1;
+  const url = "/" + month + "/" + date.year();
 
   return <Link to={url}>{str}</Link>;
 }
