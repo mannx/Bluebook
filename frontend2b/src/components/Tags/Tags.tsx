34af7674d3f5e14ac168d9191844b3033c393c32
Mannx <18175956+mannx@users.noreply.github.com>
2025-07-15 17:08:06 -0300

    frontend dep upgrade

--- frontend2b/src/components/Tags/Tags.tsx
+++ frontend2b/src/components/Tags/Tags.tsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, useLoaderData } from "react-router-dom";
 
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 
 import Table from "@mui/material/Table";
