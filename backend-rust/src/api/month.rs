ad886104f2ab0c7d1e53124394ca77130e8c122f
mannx <18175956+mannx@users.noreply.github.com>
2025-07-16 12:41:58 -0300

    .

--- backend-rust/src/api/month.rs
+++ backend-rust/src/api/month.rs
@@ -145,8 +145,7 @@ fn calculate_week_ending(
         .expect("invalid date provided");
 
     let results: Vec<DayData> = day_data
-        .filter(DayDate.ge(start_date).and(DayDate.le(data.DayDate)))
-        .filter(Updated.eq(false))
+        .filter(DayDate.ge(start_date).and(DayDate.le(data.DayDate)).and(Updated.eq(false)))
         .select(DayData::as_select())
         .load(conn)?;
 
