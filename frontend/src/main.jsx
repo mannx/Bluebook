d91bc5096f9549c2b6a55e44a32cdea332ae1414
Mannx <18175956+mannx@users.noreply.github.com>
2026-03-08 15:24:06 -0300

    react-router update p1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -162,6 +162,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         router={router}
         future={{
           v7_startTransition: true,
+          v7_fetcherPersist: true,
+          v7_normalizeFormMethod: true,
+          v7_partialHydration: true,
+          v7_skipActionErrorRevalidation: true,
         }}
       />
     </LocalizationProvider>
